0c6f6418307e99ab2a71b4e46a1aeb570bee946c
Synizter <g.pongth@gmail.com>
2017-10-25 02:39:48 +0700

    add example

--- Project/STM32L152-EXAMPLE/MDK-ARM/Objects/stm32l1xx_hal_gpio.d
+++ Project/STM32L152-EXAMPLE/MDK-ARM/Objects/stm32l1xx_hal_gpio.d
@@ -31,6 +31,8 @@
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_flash_ramfunc.h
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_pwr.h
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_pwr_ex.h
+.\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_spi.h
+.\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_spi_ex.h
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_tim.h
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_tim_ex.h
 .\objects\stm32l1xx_hal_gpio.o: ..\..\..\Drivers\STM32L1xx_HAL_Driver\Inc\stm32l1xx_hal_uart.h
